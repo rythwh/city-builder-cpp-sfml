f5dea51b7b5f959977df9de9ef69c38b1a31c7e5
Ryan White <6309098+rythwh@users.noreply.github.com>
2025-11-20 20:26:46 +0100

    Added simple window management.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,31 @@
 #include <SFML/Graphics.hpp>
+#include <optional>
+
+#include "window_utils.hpp"
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "City Builder");
     window.setFramerateLimit(144);
+    window_utils::placeWindowOnScreen(window);
 
     while (window.isOpen())
     {
-        while (const std::optional event = window.pollEvent())
+        while (const std::optional<sf::Event> event = window.pollEvent())
         {
+            // Close window with X-button
             if (event->is<sf::Event::Closed>())
             {
                 window.close();
+            }
+
+            // Close window with Escape key
+            if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+            {
+                if (keyPressed->scancode == sf::Keyboard::Scan::Escape)
+                {
+                    window.close();
+                }
             }
         }
 
---
+++ src/window_utils.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <SFML/Graphics/RenderWindow.hpp>
+
+namespace window_utils {
+    void placeWindowOnScreen(sf::RenderWindow& window);
+} // namespace window_utils
